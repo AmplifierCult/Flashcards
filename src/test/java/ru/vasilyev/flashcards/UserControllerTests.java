82af6bd29b8fbcd95d58d13d3db9412c88f5ac64
Andrey Vasilev <tribotechnique@mail.ru>
2021-11-13 13:55:51 +0300

    Update UserController test.

--- src/test/java/ru/vasilyev/flashcards/UserControllerTests.java
+++ src/test/java/ru/vasilyev/flashcards/UserControllerTests.java
@@ -1,10 +1,10 @@
 package ru.vasilyev.flashcards;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.test.web.servlet.MockMvc;
 import ru.vasilyev.flashcards.repository.UserRepository;
 
@@ -16,6 +16,9 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 public class UserControllerTests {
 
     @Autowired
+    private ObjectMapper objectMapper;
+
+    @Autowired
     UserRepository repository;
 
     @Autowired
@@ -23,6 +26,15 @@ public class UserControllerTests {
 
     @Test
     void userControllerRequests() throws Exception {
+
         this.mockMvc.perform(get("/users")).andExpect(status().isOk());
+
+
+//        this.mockMvc.perform(post("path/to/insert/student"))
+//                .accept(MediaType.APPLICATION_JSON)
+//                .andExpect(status().isOk())
+//                .andExpect(content().string("{}"));;
+
+
     }
 }
